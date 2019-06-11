18501908e9894457ef8d01502ce520e6d413558b
Tony Baker <this.tonybaker@gmail.com>
2019-06-11 23:26:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
     "#{@name_reversed}"
   end
 
-  get 'square/:number' do
+  get '/square/:number' do
     @number = params[:number].to_i
     "#{@number * @number}"
   end
