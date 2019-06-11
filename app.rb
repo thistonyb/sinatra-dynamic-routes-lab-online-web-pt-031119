9402c0d3a274af99be5dbba3e9a823295ec22096
Tony Baker <this.tonybaker@gmail.com>
2019-06-11 23:49:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -23,4 +23,11 @@ class App < Sinatra::Base
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @number1 = params[:number1]
+    @number2 = params[:number2]
+    "#{@number1} #{@operation} #{@number2}"
+  end
+
 end
